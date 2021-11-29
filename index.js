fba8efe28a226630810bd65cb5c8497263e6f12b
amoghjalan <amoghjalan2005@gmail.com>
2021-11-29 20:58:19 +0530

    Production env

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.use("/api/users", require("./routes/users"));
 
 const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT, "0.0.0.0", () =>
   console.log(`Server started on post ${PORT}`)
 );
